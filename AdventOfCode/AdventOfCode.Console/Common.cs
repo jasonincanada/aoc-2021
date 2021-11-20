05cdb9988b4eb1dcc2309cfb7a6ec2b901cab0ac
jasonincanada <nirgle@gmail.com>
2021-11-20 03:52:07 -0500

    Add console project, day 1 from 2020

---
+++ AdventOfCode/AdventOfCode.Console/Common.cs
@@ -0,0 +1,27 @@
+﻿namespace AdventOfCode
+{
+    public static class Common
+    {
+        public static List<string> GetInput(int day)
+        {
+            string filename = $"Input{day}.txt";
+
+            // Try these folders for the input file
+            List<string> folders = new()
+            {
+                @"",
+                @"..\..\..\",
+            };
+
+            foreach (var folder in folders)
+            {
+                var file = $"{folder}{filename}";
+
+                if (File.Exists(file))
+                    return File.ReadAllLines(file).ToList();
+            }
+
+            throw new Exception($"Couldn't locate input file {filename} for day {day}");
+        }
+    }
+}
---
+++ AdventOfCode/AdventOfCode.Console/Program.cs
@@ -0,0 +1,19 @@
+﻿using AdventOfCode;
+
+// Parsing
+List<int> numbers = Common.GetInput(1)
+    .Select(int.Parse)
+    .ToList();
+
+// Solve day 1, part 1 from last year (2020)
+foreach (var m in numbers)
+{
+    foreach (var n in numbers)
+    {
+        if (m + n == 2020)
+        {
+            Console.WriteLine("Part 1: {0}", m * n);
+        }
+    }
+}
+
