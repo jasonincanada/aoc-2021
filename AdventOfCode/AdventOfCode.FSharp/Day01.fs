1296535fb56bf15d865dbdf9722ba291ccd72883
jasonincanada <nirgle@gmail.com>
2021-12-02 10:05:27 -0500

    Remove FSharp project

--- AdventOfCode/AdventOfCode.FSharp/Day01.fs
+++
@@ -1,6 +0,0 @@
-﻿module AdventOfCode.FSharp.Day01
-    
-let doCase (numbers : int list) : int list = 
-    [ for m in numbers do
-        for n in numbers do
-            if m + n = 2020 then yield m*n ]
