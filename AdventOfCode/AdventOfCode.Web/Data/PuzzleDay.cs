f6b099154103c7fd519f6af3cac9d5d55f9357ee
jasonincanada <nirgle@gmail.com>
2021-12-11 12:01:44 -0500

    pages for days 1/11

---
+++ AdventOfCode/AdventOfCode.Web/Data/PuzzleDay.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AdventOfCode.Web.Data
+{
+    public class PuzzleDay
+    {
+        [Required(ErrorMessage = "Input is required")]
+        public string Input { get; set; }
+
+        public string Part1 { get; set; }
+        public string Part2 { get; set; }
+
+        public PuzzleDay()
+        {
+            Input = "paste input here";
+            Part1 = string.Empty;
+            Part2 = string.Empty;
+        }
+    }
+}
